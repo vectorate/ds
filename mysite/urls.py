5986b50e46a65af7a8a75765bbf076bd0922712e
Moabi <moabi@wirels-linux-002.(none)>
2013-08-16 14:23:01 +0200

    first commit

---
+++ mysite/urls.py
@@ -0,0 +1,31 @@
+from django.conf.urls import patterns, include, url
+from django.contrib.auth.views import login, logout
+from django.views.generic import TemplateView
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'mysite.views.home', name='home'),
+    # url(r'^mysite/', include('mysite.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+   url(r'^admin/', include(admin.site.urls)),
+   url(r'^polls/', include('polls.urls', namespace="polls")),
+
+   # User Accounts
+   #url(r'^accounts/login/$', login),
+   #url(r'^accounts/logout/$', logout),
+   #url(r'^accounts/profile/$'),
+   
+   # used for email activation
+   #url(r'^accounts/', include('registration.backends.default.urls')),
+
+   url(r'^accounts/', include('registration.backends.simple.urls')),
+   url(r'^accounts/profile/', TemplateView.as_view(template_name="profile.html")),
+)
---
+++ polls/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.utils import timezone
+import datetime
+
+
+class Poll(models.Model):
+    question = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __unicode__(self):
+        return self.question
+
+	# def was_published_recently(self):
+	#     now = timezone.now()
+	#     return ((now - datetime.timedelta(days=1)) <= self.pub_date <  now) 
+    def was_published_recently(self):
+        #return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        return ((timezone.now() - datetime.timedelta(days=1)) <= self.pub_date <  timezone.now()) 
+
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return self.choice_text
